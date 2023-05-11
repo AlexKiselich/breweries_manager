d364aa87d69271bd6668fed686af919ee0c9b60c
Alex Kiselich <alexander.kiselich@gmail.com>
2023-05-11 14:40:21 -0600

    Created index and show page for breweries. Also created tests

---
+++ spec/features/breweries/show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "/breweries/:id" do
+  describe 'as a visitor, when  I visit the brewery show page' do
+    before :each do
+      @omf = Brewery.create!(name: "OMF", draft_lines: 8, serves_food: false)
+      @ratio = Brewery.create!(name: "Ration", draft_lines: 10, serves_food: false)
+    end
+    it "displays the brewery's attributes" do
+      visit "/breweries/#{@omf.id}"
+      save_and_open_page
+      expect(page).to have_content(@omf.name)
+    end
+  end
+end
